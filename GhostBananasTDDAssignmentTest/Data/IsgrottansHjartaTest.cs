0f715cd5aa2a7b5615d0a651e86e54d680256d3c
Meganselvidge <meganselvidge@gmail.com>
2024-12-19 10:58:31 +0100

    WIP lagt till test project och nuggets men måste få testerna att fungera

---
+++ GhostBananasTDDAssignmentTest/Data/IsgrottansHjartaTest.cs
@@ -0,0 +1,15 @@
+
+using Xunit;
+
+namespace GhostBananasTDDAssignmentTest.Data;
+
+[TestSubject(typeof(IsgrottansHjarta))]
+public class IsgrottansHjartaTest
+{
+
+    [Fact]
+    public void METHOD()
+    {
+        
+    }
+}
